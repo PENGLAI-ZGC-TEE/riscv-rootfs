38cb851f0ff050a73082beabdda17af0ef8be640
Yang Bicheng <superybc@outlook.com>
2023-09-21 16:25:09 +0800

    cma: use cma to allocate enclave memory

--- apps/penglai-driver/penglai-enclave-driver.h
+++ apps/penglai-driver/penglai-enclave-driver.h
@@ -2,5 +2,15 @@
 #define _PENGLAI_ENCLAVE_DRIVER
 
 #define DEFAULT_SECURE_PAGES_ORDER 10
+/* the size of cma area */
+#define CMA_SHIFT	30
+#define CMA_SIZE    (1UL << CMA_SHIFT) // 1 GB
+
+typedef struct cma_mem
+{
+    void           *cma_start_va;
+	phys_addr_t     cma_start_pa;
+    dma_addr_t      cma_handle;  // bus address
+} cma_mem_t;
 
 #endif
